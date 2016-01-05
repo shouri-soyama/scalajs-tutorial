60255bb7f4361b6979bd94d150e141b2b8b3a952
S.Soyama <s_soyama@denkiyagi.jp>
2016-01-05 17:50:01 +0900

    add button

--- build.sbt
+++ build.sbt
@@ -4,4 +4,6 @@ scalaJSStage in Global := FastOptStage
 
 name := "Scala.js Tutorial"
 
-scalaVersion := "2.11.7" // or any other Scala version >= 2.10.2
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,9 +1,22 @@
 package tutorial.webapp
 
+import org.scalajs.dom
+import dom.document
+import scala.scalajs.js.annotation.JSExport
 import scala.scalajs.js.JSApp
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
-    println("Hello world!")
+    appendPar(document.body, "Hello World")
+  }
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+  @JSExport
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
   }
 }
