23fbae98be08067c3c7fc44bd4332c63edbe4901
S.Soyama <s_soyama@denkiyagi.jp>
2016-01-06 16:18:11 +0900

    Ajaxの追加

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -3,6 +3,7 @@ package tutorial.webapp
 import org.scalajs.dom
 import org.scalajs.jquery.jQuery
 import dom.document
+import dom.ext.Ajax
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
 import scala.scalajs.js.JSApp
@@ -15,7 +16,7 @@ object TutorialApp extends JSApp {
     js.Dynamic.global.$("#click-me-button").on("click", addClickedMessage _)
 //    js.Dynamic.global.addClick = addClickedMessage _
 //    js.Dynamic.global.Array.prototype.addClick = addClickedMessage _
-    
+    js.Dynamic.global.$(document.body).keydown((e: dom.KeyboardEvent) => println(e.keyCode))
   }
   def appendPar(targetNode: dom.Node, text: String): Unit = {
     val parNode = document.createElement("p")
@@ -28,5 +29,8 @@ object TutorialApp extends JSApp {
     val mydata = js.Dynamic.global.mydata
     js.Dynamic.global.console.log("aaaaaaa")
     appendPar(document.body, mydata.msg.asInstanceOf[String])
+    import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
+    val url = "http://api.openweathermap.org/data/2.5/weather?q=Singapore"
+    Ajax.get(url).onSuccess{ case xhr => appendPar(document.body, xhr.responseText) }
   }
 }
