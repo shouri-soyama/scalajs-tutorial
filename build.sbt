7971ee087800a188964933c3f58947375bcbd558
S.Soyama <s_soyama@denkiyagi.jp>
2016-01-06 13:22:08 +0900

    global経由の操作

--- build.sbt
+++ build.sbt
@@ -6,4 +6,7 @@ name := "Scala.js Tutorial"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
+libraryDependencies ++= Seq(
+  "org.scala-js" %%% "scalajs-dom" % "0.8.0",
+  "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
+)
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,13 +1,21 @@
 package tutorial.webapp
 
 import org.scalajs.dom
+import org.scalajs.jquery.jQuery
 import dom.document
+import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
 import scala.scalajs.js.JSApp
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
     appendPar(document.body, "Hello World")
+    js.eval("Array.prototype.fuga = function() { console.log(this) }")
+    js.Dynamic.global.hoge = "test"
+    js.Dynamic.global.$("#click-me-button").on("click", addClickedMessage _)
+//    js.Dynamic.global.addClick = addClickedMessage _
+//    js.Dynamic.global.Array.prototype.addClick = addClickedMessage _
+    
   }
   def appendPar(targetNode: dom.Node, text: String): Unit = {
     val parNode = document.createElement("p")
@@ -15,8 +23,10 @@ object TutorialApp extends JSApp {
     parNode.appendChild(textNode)
     targetNode.appendChild(parNode)
   }
-  @JSExport
-  def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+  def addClickedMessage(e: js.Any): Unit = {
+    js.Dynamic.global.console.dir(e)
+    val mydata = js.Dynamic.global.mydata
+    js.Dynamic.global.console.log("aaaaaaa")
+    appendPar(document.body, mydata.msg.asInstanceOf[String])
   }
 }
